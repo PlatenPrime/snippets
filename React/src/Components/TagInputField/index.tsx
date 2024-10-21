4e9ed7dcbcad9d527ec6f76afab6d87c8a909ebe
Aleksandr Marchenko <proshta@gmail.com>
2024-10-21 14:56:50 +0300

    21.10.2024

---
+++ React/src/Components/TagInputField/index.tsx
@@ -0,0 +1,43 @@
+
+import { useState } from "react";
+import "./index.css"
+
+
+
+export const TagInput = ({ tags }  ) => {
+    const [tagData, setTagData] = useState(tags);
+
+    const removeTagData = indexToRemove => {
+        setTagData([...tagData.filter((_, index) => index !== indexToRemove)]);
+    };
+
+    const addTagData = (event : any)  => {
+        if (event.target.value !== '') {
+            setTagData([...tagData, event.target.value]);
+            event.target.value = '';
+        }
+    };
+
+    return (
+        <div className="tag-input">
+            <ul className="tags">
+                {tagData.map((tag: string, index: number) => (
+                    <li key={index} className="tag">
+                        <span className="tag-title">{tag}</span>
+                        <span
+                            className="tag-close-icon"
+                            onClick={() => removeTagData(index)}
+                        >
+                            x
+                        </span>
+                    </li>
+                ))}
+            </ul>
+            <input
+                type="text"
+                onKeyUp={event => (event.key === 'Enter' ? addTagData(event) : null)}
+                placeholder="Press enter to add a tag"
+            />
+        </div>
+    );
+};
---
+++ React/src/app/App.tsx
@@ -0,0 +1,14 @@
+import { TagInput } from "../Components/TagInputField"
+
+
+function App() {
+
+
+  return (
+    <>
+      <TagInput tags={['Nodejs', 'MongoDB']} />
+    </>
+  )
+}
+
+export default App
